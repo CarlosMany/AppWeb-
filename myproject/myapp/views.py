e1562867b7305a98a28816be0a8e08788691bf7f
CarlosMany <carlosmany12@hotmail.com>
2024-06-27 20:36:30 -0600

    Feat: Modificación
    Se agregaron campos y modificaciones respecto a las adaptaciones

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
 from .forms import ItemForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
 
 def item_list(request):
     items = Item.objects.all()
@@ -37,3 +40,32 @@ def item_delete(request, pk):
     item = get_object_or_404(Item, pk=pk)
     item.delete()
     return redirect('item_list')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if user.is_staff:
+                return redirect('admin_dashboard')
+            else:
+                return redirect('user_dashboard')
+        else:
+            return HttpResponse("Invalid login")
+    return render(request, 'login.html')
+
+def admin_dashboard(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    items = Item.objects.all()
+    return render(request, 'admin_dashboard.html', {'items': items})
+
+def user_dashboard(request):
+    if not request.user.is_authenticated or request.user.is_staff:
+        return redirect('login')
+    return render(request, 'user_dashboard.html', {'user': request.user})
+
+def main_view(request):
+    return render(request, 'main.html')
