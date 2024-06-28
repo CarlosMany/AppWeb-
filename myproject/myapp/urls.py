2e6118845ba9873a8ebd81e16258f0f092c3ba1a
CarlosMany <carlosmany12@hotmail.com>
2024-06-27 20:34:29 -0600

    Feat: Modificación
    Se agregaron campos y modificaciones respecto a las adaptaciones

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,9 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_list, name='item_list'),
+    path('', views.login_view, name='login'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
+    # Existing paths...
     path('item/<int:pk>/', views.item_detail, name='item_detail'),
     path('item/new/', views.item_new, name='item_new'),
     path('item/<int:pk>/edit/', views.item_edit, name='item_edit'),
     path('item/<int:pk>/delete/', views.item_delete, name='item_delete'),
+    #codigo agregado
+    path('', views.main_view, name='main'),
+    path('login/', views.login_view, name='login'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
 ]
