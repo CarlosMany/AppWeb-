747ae772494b5bb85e11533df3106534bab85b7d
CarlosMany <carlosmany12@hotmail.com>
2024-06-30 17:32:12 -0600

    Feat: Adaptaciones

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('item/new/', views.item_new, name='item_new'),
     path('item/<int:pk>/edit/', views.item_edit, name='item_edit'),
     path('item/<int:pk>/delete/', views.item_delete, name='item_delete'),
+    path('items/', views.item_list, name='item_list'),
     #codigo agregado
     path('', views.main_view, name='main'),
     path('login/', views.login_view, name='login'),
